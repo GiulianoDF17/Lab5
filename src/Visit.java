0688ae9bf28202e2fe8f2961e58be6bd216925cf
GiulianoDF17 <2578593@edu.vaniercollege.qc.ca>
2026-03-02 10:56:14 -0500

    Added Customer + DiscountRate + Visit

---
+++ src/Visit.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+public class Visit {
+
+    private Customer customer;
+    private Date date;
+    private double serviceExpense;
+    private double productExpense;
+
+    public Visit(String name, Date date){
+        this.customer = new Customer(name);
+        this.date = date;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public String getName(){
+        return customer.getName();
+    }
+
+    public double getServiceExpense(){
+        return serviceExpense;
+    }
+
+    public void setServiceExpense(double ex){
+        this.serviceExpense = ex;
+    }
+
+    public double getProductExpense(){
+        return productExpense;
+    }
+
+    public void setProductExpense(double ex){
+        this.productExpense = ex;
+    }
+
+    public double getTotalExpense(){
+        double totalExpense = productExpense + serviceExpense;
+        return totalExpense;
+    }
+
+    @Override
+    public String toString(){
+        return "Visit[customer=" + customer + ", date=" + date ", totalExpense=" + getTotalExpense() + "]";
+    }
+}
